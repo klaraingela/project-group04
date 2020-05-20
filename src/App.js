71aaa4a55315b2d146eac2c273ff0bc92c9df99e
Sandra Smrekar <sandrasmrekar98@gmail.com>
2020-05-20 09:22:17 +0200

    Bytat api, fixat design för superhjältar, och en fungerande sökfunktion

--- src/App.js
+++ src/App.js
@@ -16,26 +16,62 @@ function Navbar(){
 }
 
 function Search(props){
+  var [input, setInput] = React.useState("");
+
+  const search= event =>{
+    if(input.length > 0){
+      props.getMovies(input);
+    }else{
+      alert("Please enter input")
+    }
+    setInput("");
+  }
+
   return(
     <div className="search-input">
       <h1>Search among 8485739 movies</h1>
-      <input placeholder="Search" onChange={event=>props.getMovies(event.target.value)}></input>
+      <div className="search-button">
+        <input placeholder="Search" onChange={event=>setInput(event.target.value)} value={input}></input>
+        <button onClick={search}>Go</button>
+      </div>
+
     </div>
   );
 }
 
+
 function Movie(props){
+  var movieUrl = props.movie.thumbnail.path +'.'+ props.movie.thumbnail.extension;
+
   return(
     <div className="card">
-      <img src="avengers.jpg" alt="Avengers"></img>
+      <img src={movieUrl} alt="Avengers"></img>
       <div className="container">
-        <h4><b>Avengers</b></h4>
-        <img src={props.image}></img>
+        <h4><b>{props.movie.name}</b></h4>
+        <img src="star_hollow.png"></img>
       </div>
    </div>
  );
 
 }
+
+function AllMovies(props){
+    var movies;
+    if(props.movies.length === 0 || props.movies.length === undefined){
+      alert("No result");
+    }else{
+        movies = props.movies.map(movie => {
+        return (
+            <Movie movie={movie}/>
+        );
+      });
+    }
+
+    return (
+        <ul className="movies">{movies}</ul>
+    )
+  }
+
 
 
 function App(){
@@ -44,37 +80,44 @@ function App(){
   const getMovies = async (title) =>{
     console.log("Search: " + title);
 
-    const response = await fetch("https://imdb8.p.rapidapi.com/title/find?q=" + title, {
+    //FILMER API
+    /*
+    const response = await fetch("https://imdb8.p.rapidapi.com/title/auto-complete?q=" + title, {
   	"method": "GET",
   	"headers": {
   		"x-rapidapi-host": "imdb8.p.rapidapi.com",
   		"x-rapidapi-key": "33f16e4f7fmsh98dd17eec3d8a2fp16ab71jsncce1b696746d"
   	}
-    })
+    })*/
 
+    //RECEPT API
+    /*
+    const response = await fetch("https://api.spoonacular.com/recipes/search?apiKey=a861207a921d4d7d9aa1d74e7f9eca4c&query="+title)
+    //const res = await response.json();
+    //setData(res.d);
+    //https://api.spoonacular.com/recipes/search??apiKey=a861207a921d4d7d9aa1d74e7f9eca4c&query=cheese&number=2
     const res = await response.json();
-    setData(res.results);
-    //console.log(res.results[0].image.url);
+    console.log(res.results);
+    setData(res.results)
+    */
+
+    //SUPERHJÄLTAR API
+    const response = await fetch("https://gateway.marvel.com:443/v1/public/characters?nameStartsWith="+title+"&apikey=13c9801495b19e2d9ac692bdfd0a2adc")
+    const res = await response.json();
+    console.log(res.data.results);
+    setData(res.data.results);
+
+    console.log("L: " + res.data.length);
 
 
 
-
-
-  }
-
-/*
-  for(var i = 0; i < data.length; i++){
-    console.log(data[i].image.url);
-  }*/
+    }
 
   return (
     <main>
       <Navbar/>
       <Search getMovies={getMovies} />
-      {data.map(data =>(
-
-        <Movie image={data.image.url} />
-      ))}
+      <AllMovies movies={data}/>
     </main>
 
 
